7ac1212dab783d017d2413eff343a9f7f937c098
Noah Roberts <noahmroberts@gmail.com>
2020-12-04 15:54:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,30 @@
-function addNewItem() {
-  // this takes the form input and adds it to the list
-  $("#js-shopping-list-form").submit(function(event) {
-  // prevent the form from attempting to submit to server
-  event.preventDefault();
-  let newItem = $('#shopping-list-entry').val();
+$(function(){
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    let listItem = $('#shopping-list-entry').val();
+
   $('.shopping-list').append(
-    `<li>
-      <span class="shopping-item">${newItem}</span>
+      `<li>
+        <span class="shopping-item">${listItem}</span>
         <div class="shopping-item-controls">
           <button class="shopping-item-toggle">
             <span class="button-label">check</span>
-          </button> 
+          </button>
           <button class="shopping-item-delete">
             <span class="button-label">delete</span>
           </button>
         </div>
-    </li>`
-  );
-  // clear the form input
-  $('#shopping-list-entry').val(' ');
-  });
-};
+      </li>`);
 
-function checkItem() {
-  // applies line-through CSS class to item
-  $("ul").on("click", ".shopping-item-toggle", function() {
-    $(this).parent().parent().find('.shopping-item').toggleClass("shopping-item__checked");
-  });
-};
-
-function deleteItem() {
-  // deletes item from list
-  $("ul").on("click", ".shopping-item-delete", function() {
-    $(event.target).closest("li").remove();
+  $('#shopping-list-entry').val('');  
   });
 
-};
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  });
 
-function shoppingListMain() {
-  addNewItem();
-  checkItem();
-  deleteItem();
-};
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
 
-$(shoppingListMain);
+});
